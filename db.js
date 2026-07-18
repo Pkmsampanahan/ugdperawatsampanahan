fe77b6ce88fda9230df51cd3ea5790e32640d540
Pkmsampanahan <uptdpkmsampanahan@gmail.com>
2026-07-19 02:22:40 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    VERSION: "v1.4.3",
+    // URL Web App GAS Anda yang baru
     WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxREItKObYbYXb_76kHzGyUfnRGDtL4xKP6ibYcwA-Io0O5I-qkkPBL8mVQg2cZy3BU0g/exec",
     DEFAULT_QR: "UGD-SAMPANAHAN-SCAN",
     MONTHS: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
